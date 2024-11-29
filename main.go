b01e7ef69f5ec218a88eb65413cf45677b8196ec
erdem <gunererd@gmail.com>
2024-11-29 23:22:46 +0300

    refactor: inject dependencies on app creation

--- main.go
+++ main.go
@@ -5,13 +5,20 @@ import (
 	"os"
 
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/gunererd/grease/internal/buffer"
 	"github.com/gunererd/grease/internal/editor"
+	"github.com/gunererd/grease/internal/highlight"
 	ioManager "github.com/gunererd/grease/internal/io"
+	"github.com/gunererd/grease/internal/ui"
 )
 
 func main() {
 	manager := ioManager.NewManager(ioManager.NewStdinSource(), ioManager.NewStdoutSink())
-	m := editor.New(manager)
+	highlightManager := highlight.NewManager()
+	buffer := buffer.New()
+	statusLine := ui.NewStatusLine()
+	viewport := ui.NewViewport(0, 0)
+	m := editor.New(manager, buffer, statusLine, viewport, highlightManager)
 
 	// Load content from stdin if it's not a terminal
 	stat, _ := os.Stdin.Stat()
